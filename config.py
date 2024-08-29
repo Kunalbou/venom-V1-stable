a2cc610fa9b7d94816c135aa85a0214b489d5dc8
Kunalbou <tsubrno@gmail.com>
2024-08-29 18:23:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # Don't use quotes( " and ' )
-#SCRIPT BY VENOMxCRAZY
+#SCRIPT BY K.T😈
   
 #Enter Your Bot Token here get it from @botfarher
 BOT_TOKEN=("TOKEN")
 
-  #Enter your group id here Get it by creating a group that group add your bot and ross bot then public it and type /id to get your id
-GROUP_ID=("GROUPid")
+  #Enter your group id here Get it by creating a group that group add your bot and ross bot then public it and type /7245486488
+GROUP_ID=("4556089868")
 
 
 
@@ -13,4 +13,3 @@ GROUP_ID=("GROUPid")
 
 
 
-  
